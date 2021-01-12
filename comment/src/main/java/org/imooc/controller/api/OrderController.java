129efa79e5e61e2bea3dff0ee878b6f06fd3983c
Jokerjin <969952727@qq.com>
2021-01-12 22:59:47 +0800

    team2 订单创建 第一天

---
+++ comment/src/main/java/org/imooc/controller/api/OrderController.java
@@ -0,0 +1,10 @@
+package org.imooc.controller.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+    public void addOrder(){
+        System.out.println("team2 订单创建 第一天");
+    }
+}
